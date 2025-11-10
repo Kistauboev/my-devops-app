1485c157a9556ea8ea83d21940f817d4e46ff7bc
usman <shyngyzusmanov04gmail.com>
2025-11-10 14:54:35 +0500

    Fix CI/CD pipeline with complete job definitions

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,26 +2,23 @@ terraform {
   required_providers {
     digitalocean = {
       source = "digitalocean/digitalocean"
-      version = "~> 2.0"
+      version = "2.26.0"
     }
   }
 }
 
-# Configure the DigitalOcean Provider
 provider "digitalocean" {
   token = var.do_token
 }
 
-# Create a droplet
 resource "digitalocean_droplet" "devops_server" {
   image  = "ubuntu-22-04-x64"
   name   = "devops-app-server"
-  region = "nyc3"
+  region = "nyc1"
   size   = "s-1vcpu-1gb"
   ssh_keys = [var.ssh_key_id]
 }
 
-# Output the server IP address
 output "server_ip" {
   value = digitalocean_droplet.devops_server.ipv4_address
 }
